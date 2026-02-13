cf6e145bb1772cfb520bdfa5e2874540fd48f900
Logan Hollowood <98189655+GearBoxFox@users.noreply.github.com>
2026-02-13 09:31:57 -0500

    test build drive code

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 3;
-  public static final String GIT_SHA = "55b9f22e0662cfad134a1b65f42f721e54557171";
-  public static final String GIT_DATE = "2026-02-01 15:27:13 EST";
+  public static final int GIT_REVISION = 4;
+  public static final String GIT_SHA = "980be831568e4ca143a9a904b446b99cb26a9ac6";
+  public static final String GIT_DATE = "2026-02-01 16:52:27 EST";
   public static final String GIT_BRANCH = "drivetrain-real";
-  public static final String BUILD_DATE = "2026-02-01 16:33:57 EST";
-  public static final long BUILD_UNIX_TIME = 1769981637371L;
+  public static final String BUILD_DATE = "2026-02-10 17:04:43 EST";
+  public static final long BUILD_UNIX_TIME = 1770761083828L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
