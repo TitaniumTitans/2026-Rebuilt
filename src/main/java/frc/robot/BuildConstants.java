757f85446de95dde9f20694077455597604f2864
Logan Hollowood <98189655+GearBoxFox@users.noreply.github.com>
2026-02-15 10:43:35 -0500

    added photonvision deps

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 4;
-  public static final String GIT_SHA = "980be831568e4ca143a9a904b446b99cb26a9ac6";
-  public static final String GIT_DATE = "2026-02-01 16:52:27 EST";
-  public static final String GIT_BRANCH = "drivetrain-real";
-  public static final String BUILD_DATE = "2026-02-10 17:04:43 EST";
-  public static final long BUILD_UNIX_TIME = 1770761083828L;
+  public static final int GIT_REVISION = 12;
+  public static final String GIT_SHA = "a333b8bb68830e905cec08a6b9c2b53548037861";
+  public static final String GIT_DATE = "2026-02-15 10:17:34 EST";
+  public static final String GIT_BRANCH = "subsystem-setup";
+  public static final String BUILD_DATE = "2026-02-15 10:36:34 EST";
+  public static final long BUILD_UNIX_TIME = 1771169794926L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
