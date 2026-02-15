fcb89c4b3895e4d83b2e332044d682c544f8c9d1
Logan Hollowood <98189655+GearBoxFox@users.noreply.github.com>
2026-02-15 10:04:22 -0500

    filled out generic intake IO

--- src/main/java/frc/robot/subsystems/intake/IntakeIO.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -1,4 +1,23 @@
 package frc.robot.subsystems.intake;
 
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Voltage;
+import org.littletonrobotics.junction.AutoLog;
+
 public interface IntakeIO {
+    @AutoLog
+    class IntakeIOInputs {
+        public double intakeVoltage = 0.0;
+        public double intakeStatorCurrent = 0.0;
+        public double intakeSupplyCurrent = 0.0;
+
+        public double pivotAngleDegrees = 0.0;
+        public double pivotVoltage = 0.0;
+        public double pivotStatorCurrent = 0.0;
+        public double pivotSupplyCurrent = 0.0;
+    }
+
+    default void setIntakeVoltage(Voltage voltage) {};
+    default void setPivotVoltage(Voltage voltage) {};
+    default void setPivotAngle(Angle angle) {};
 }
