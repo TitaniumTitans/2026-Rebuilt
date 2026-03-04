5d41d0aeca8265299f1a7866c947a0a4f34a116e
Logan Hollowood <98189655+GearBoxFox@users.noreply.github.com>
2026-03-04 16:05:27 -0500

    cleaned up robot container and added comments

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,12 +1,11 @@
 // Copyright (c) FIRST and other WPILib contributors.
-
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
 
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.gos.lib.properties.PropertyManager;
+import static edu.wpi.first.units.Units.Volts;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -27,118 +26,158 @@ import frc.robot.subsystems.shooter.ShooterIOTalonFX;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
 import frc.robot.subsystems.vision.*;
 
-import static edu.wpi.first.units.Units.Volts;
-
-
+/**
+ * Container for robot subsystems, commands, and button bindings.
+ * Instantiates subsystems based on robot mode (REAL/SIM/REPLAY).
+ */
 public class RobotContainer {
-    private final CommandXboxController driveController = new CommandXboxController(0);
+  // Driver controller
+  private final CommandXboxController driveController = new CommandXboxController(0);
 
-    private final DriveSubsystem swerve;
-    private final ShooterSubsystem shooter;
-    private final IntakeSubsystem intake;
-    private final FeederSubsystem feeder;
-    private final VisionSubsystem vision;
+  // Subsystems
+  private final DriveSubsystem swerve;
+  private final ShooterSubsystem shooter;
+  private final IntakeSubsystem intake;
+  private final FeederSubsystem feeder;
+  private final VisionSubsystem vision;
 
-    public RobotContainer()
-    {
-        switch (Constants.getMode()) {
-          case REAL -> {
-              swerve = new DriveSubsystem(
-                  new GyroIOPigeon2(),
-                  new ModuleIOTalonFX(DriveConstants.MODULE_CONSTANTS[0]),
-                  new ModuleIOTalonFX(DriveConstants.MODULE_CONSTANTS[1]),
-                  new ModuleIOTalonFX(DriveConstants.MODULE_CONSTANTS[2]),
-                  new ModuleIOTalonFX(DriveConstants.MODULE_CONSTANTS[3])
-              );
+  /**
+   * Constructs the robot container.
+   * Initializes subsystems based on the robot mode and configures bindings.
+   */
+  public RobotContainer() {
+    // Initialize subsystems based on robot mode
+    switch (Constants.getMode()) {
+      case REAL -> {
+        // Real robot hardware
+        swerve = new DriveSubsystem(
+          new GyroIOPigeon2(),
+          new ModuleIOTalonFX(DriveConstants.MODULE_CONSTANTS[0]),
+          new ModuleIOTalonFX(DriveConstants.MODULE_CONSTANTS[1]),
+          new ModuleIOTalonFX(DriveConstants.MODULE_CONSTANTS[2]),
+          new ModuleIOTalonFX(DriveConstants.MODULE_CONSTANTS[3])
+        );
 
-              shooter = new ShooterSubsystem(new ShooterIOTalonFX());
-              intake = new IntakeSubsystem(new IntakeIOTalonFX());
-              feeder = new FeederSubsystem(new FeederIOTalonFX());
-              vision = new VisionSubsystem(VisionConstants.FILTER_PARAMETERS,
-                      new VisionIOPhotonReal("Shooter Left", VisionConstants.LEFT_CAMERA_TRANSFORM, FieldConstants.defaultAprilTagType.getLayout())
-              );
-          }
-          case SIM -> {
-              swerve = new DriveSubsystem(
-                  new GyroIOSim(),
-                  new ModuleIO() {},
-                  new ModuleIO() {},
-                  new ModuleIO() {},
-                  new ModuleIO() {}
-              );
-
-              shooter = new ShooterSubsystem(new ShooterIO() {});
-              intake = new IntakeSubsystem(new IntakeIO() {});
-              feeder = new FeederSubsystem(new FeederIO() {});
-              vision = new VisionSubsystem(VisionConstants.FILTER_PARAMETERS,
-                      new VisionIOPhotonSimulation("Shooter left",
-                              VisionConstants.LEFT_CAMERA_TRANSFORM,
-                              FieldConstants.defaultAprilTagType.getLayout(),
-                              VisionConstants.SIM_CAMERA_PROPERTIES)
-              );
-          }
-          case REPLAY -> {
-              swerve = new DriveSubsystem(
-                  new GyroIO() {},
-                  new ModuleIO() {},
-                  new ModuleIO() {},
-                  new ModuleIO() {},
-                  new ModuleIO() {}
-              );
-
-              shooter = new ShooterSubsystem(new ShooterIO() {});
-              intake = new IntakeSubsystem(new IntakeIO() {});
-              feeder = new FeederSubsystem(new FeederIO() {});
-              vision = new VisionSubsystem(VisionConstants.FILTER_PARAMETERS, new VisionIO() {});
-          }
-          default -> throw new IllegalStateException("Unexpected value: " + Constants.getMode());
-        }
-
-        configureBindings();
-        configureDashboardCommands();
-    }
-    
-    
-    private void configureBindings() {
-      swerve.setDefaultCommand(
-          DriveCommands.joystickDrive(
-            swerve,
-            () -> driveController.getLeftY(),
-            () -> driveController.getLeftX(),
-            () -> -driveController.getRightX()
+        shooter = new ShooterSubsystem(new ShooterIOTalonFX());
+        intake = new IntakeSubsystem(new IntakeIOTalonFX());
+        feeder = new FeederSubsystem(new FeederIOTalonFX());
+        vision = new VisionSubsystem(
+          VisionConstants.FILTER_PARAMETERS,
+          new VisionIOPhotonReal(
+            "Shooter Left",
+            VisionConstants.LEFT_CAMERA_TRANSFORM,
+            FieldConstants.defaultAprilTagType.getLayout()
           )
-      );
+        );
+      }
+      case SIM -> {
+        // Simulated hardware
+        swerve = new DriveSubsystem(
+          new GyroIOSim(),
+          new ModuleIO() {},
+          new ModuleIO() {},
+          new ModuleIO() {},
+          new ModuleIO() {}
+        );
 
-      driveController.start().onTrue(
-          Commands.runOnce(() -> RobotState.getInstance().resetPose(new Pose2d()))
-      );
+        shooter = new ShooterSubsystem(new ShooterIO() {});
+        intake = new IntakeSubsystem(new IntakeIO() {});
+        feeder = new FeederSubsystem(new FeederIO() {});
+        vision = new VisionSubsystem(
+          VisionConstants.FILTER_PARAMETERS,
+          new VisionIOPhotonSimulation(
+            "Shooter left",
+            VisionConstants.LEFT_CAMERA_TRANSFORM,
+            FieldConstants.defaultAprilTagType.getLayout(),
+            VisionConstants.SIM_CAMERA_PROPERTIES
+          )
+        );
+      }
+      case REPLAY -> {
+        // Log replay mode (no hardware)
+        swerve = new DriveSubsystem(
+          new GyroIO() {},
+          new ModuleIO() {},
+          new ModuleIO() {},
+          new ModuleIO() {},
+          new ModuleIO() {}
+        );
 
-      driveController.povUp().onTrue(shooter.setHoodPosition(0.3));
-      driveController.povDown().onTrue(shooter.setHoodPosition(0.1));
-
-      driveController.a().whileTrue(shooter.runDashboardRPM());
-      driveController.b().whileTrue(feeder.runFeeder(Volts.of(12.0)));
-
-      driveController.x().onTrue(intake.homingCommand());
-
-      driveController.rightBumper().whileTrue(intake.intake());
-      driveController.leftBumper().onTrue(intake.setPivotPosition(IntakeSubsystem.Position.STOWED));
+        shooter = new ShooterSubsystem(new ShooterIO() {});
+        intake = new IntakeSubsystem(new IntakeIO() {});
+        feeder = new FeederSubsystem(new FeederIO() {});
+        vision = new VisionSubsystem(VisionConstants.FILTER_PARAMETERS, new VisionIO() {});
+      }
+      default -> throw new IllegalStateException("Unexpected value: " + Constants.getMode());
     }
-    
-    
-    public Command getAutonomousCommand()
-    {
-        return intake.homingCommand();
-    }
 
-    public void configureDashboardCommands() {
-        // manually control the pivot hood
-        SmartDashboard.putData("Hood to %10", shooter.setHoodPosition(0.1));
-        SmartDashboard.putData("Hood to %30", shooter.setHoodPosition(0.3));
-        SmartDashboard.putData("Hood to %50", shooter.setHoodPosition(0.5));
-        SmartDashboard.putData("Hood to %70", shooter.setHoodPosition(0.7));
-        SmartDashboard.putData("Hood to %90", shooter.setHoodPosition(0.9));
+    configureBindings();
+    configureDashboardCommands();
+  }
 
-        SmartDashboard.putData("Reset Field Oriented", Commands.runOnce(() -> RobotState.getInstance().resetPose(new Pose2d())).withName("Reset Field Orient"));
-    }
+  /**
+   * Configures driver controller button bindings.
+   */
+  private void configureBindings() {
+    // Default drive command: joystick control
+    swerve.setDefaultCommand(
+      DriveCommands.joystickDrive(
+        swerve,
+        () -> driveController.getLeftY(),
+        () -> driveController.getLeftX(),
+        () -> -driveController.getRightX()
+      )
+    );
+
+    // Start button: reset robot pose to origin
+    driveController.start().onTrue(
+      Commands.runOnce(() -> RobotState.getInstance().resetPose(new Pose2d()))
+    );
+
+    // D-pad: adjust shooter hood angle
+    driveController.povUp().onTrue(shooter.setHoodPosition(0.3));
+    driveController.povDown().onTrue(shooter.setHoodPosition(0.1));
+
+    // A button: run shooter at dashboard RPM
+    driveController.a().whileTrue(shooter.runDashboardRPM());
+
+    // B button: run feeder at 12V
+    driveController.b().whileTrue(feeder.runFeeder(Volts.of(12.0)));
+
+    // X button: home the intake
+    driveController.x().onTrue(intake.homingCommand());
+
+    // Right bumper: deploy and run intake
+    driveController.rightBumper().whileTrue(intake.intake());
+
+    // Left bumper: stow intake
+    driveController.leftBumper().onTrue(intake.setPivotPosition(IntakeSubsystem.Position.STOWED));
+  }
+
+  /**
+   * Returns the autonomous command.
+   * Currently configured to home the intake.
+   */
+  public Command getAutonomousCommand() {
+    return intake.homingCommand();
+  }
+
+  /**
+   * Adds manual control commands to the SmartDashboard for testing.
+   */
+  public void configureDashboardCommands() {
+    // Manual hood position control
+    SmartDashboard.putData("Hood to %10", shooter.setHoodPosition(0.1));
+    SmartDashboard.putData("Hood to %30", shooter.setHoodPosition(0.3));
+    SmartDashboard.putData("Hood to %50", shooter.setHoodPosition(0.5));
+    SmartDashboard.putData("Hood to %70", shooter.setHoodPosition(0.7));
+    SmartDashboard.putData("Hood to %90", shooter.setHoodPosition(0.9));
+
+    // Reset field-oriented driving
+    SmartDashboard.putData(
+      "Reset Field Oriented",
+      Commands.runOnce(() -> RobotState.getInstance().resetPose(new Pose2d()))
+        .withName("Reset Field Orient")
+    );
+  }
 }
