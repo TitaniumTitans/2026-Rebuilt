faaaf0badbd0411c8d3dc712188267f57187ee1e
Logan Hollowood <98189655+GearBoxFox@users.noreply.github.com>
2026-03-08 14:37:17 -0400

    clean up imports

--- src/main/java/frc/robot/auto/AutoSelector.java
+++ src/main/java/frc/robot/auto/AutoSelector.java
@@ -1,11 +1,10 @@
 package frc.robot.auto;
 
 import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.path.PathPlannerPath;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.drive.DriveSubsystem;
-import frc.robot.util.AllianceFlipUtil;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 public class AutoSelector {
