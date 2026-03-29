3b3d1c61d55ee21a164d7fa8adf53c5f37006262
Logan Hollowood <98189655+GearBoxFox@users.noreply.github.com>
2026-03-29 19:17:27 -0400

    left side auto flesh out

---
+++ src/main/java/frc/robot/commands/LeftCenterAutoCommandGroup.java
@@ -0,0 +1,21 @@
+package frc.robot.commands;
+
+
+import com.pathplanner.lib.auto.NamedCommands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.intake.IntakeSubsystem;
+
+public class LeftCenterAutoCommandGroup extends SequentialCommandGroup {
+  public LeftCenterAutoCommandGroup(Drive drive, IntakeSubsystem intake) {
+    super(drive.followPathCustomCommand("LeftBumpSend"),
+        intake.setPivotPosition(IntakeSubsystem.Position.INTAKE),
+//        Commands.waitSeconds(1.0),
+        intake.setIntakePower(IntakeSubsystem.Speed.INTAKE),
+        drive.followPathCommand("LeftSweepOne"),
+//        intake.setIntakePower(IntakeSubsystem.Speed.STOP),
+//        Commands.waitSeconds(1.0),
+        drive.followPathCustomCommand("LeftBumpReturn"),
+        NamedCommands.getCommand("aimAndShoot"));
+  }
+}
